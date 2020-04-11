d9cd16a6866af9166f46397d0363c380c5b9c8c5
abdennour001 <amokranabdennour@gmail.com>
2020-04-11 16:52:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mac/macapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mac.macapp
+
+import android.os.Build
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import java.util.*
+import kotlin.math.abs
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
